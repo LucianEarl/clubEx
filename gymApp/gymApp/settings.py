f7e933f7c1a30656bfef475fa35cd788827113b5
Dan Hayworth <dan.d.hayworth@gmail.com>
2020-11-07 19:48:13 +1300

    gym database

--- gymApp/gymApp/settings.py
+++ gymApp/gymApp/settings.py
@@ -68,7 +68,7 @@ DATABASES = {
         'CONN_MAX_AGE': 0,
         'ENGINE': 'django.db.backends.sqlite3',
         'HOST': 'localhost',
-        'NAME': 'project.db',
+        'NAME': 'gym.db',
         'PASSWORD': '',
         'PORT': '',
         'USER': ''
