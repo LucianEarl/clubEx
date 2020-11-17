31c75d98299e7940a3670b9134ca60a1edb15185
Dan Hayworth <dan.d.hayworth@gmail.com>
2020-11-17 16:22:40 +1300

    restore

--- gymApp/clubEx/models.py
+++ gymApp/clubEx/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CategoryManager(models.Manager):
