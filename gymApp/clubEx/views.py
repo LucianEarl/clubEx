bca3b8621024357071c03bcb510a5e1a10677899
Lyderies <Lyderies@hotmail.com>
2020-11-18 22:44:50 +1300

    corrections to views file for video

--- gymApp/clubEx/views.py
+++ gymApp/clubEx/views.py
@@ -73,17 +73,17 @@ class VideoDetailView(DetailView):
     model = Exercise
     template_name = 'video.html'
 
-    def VideoViews(self): # testing for video views
+    # def VideoViews(self): # testing for video views
 
-        if(Exercise.views.objects.count()<=0):
-            x=Exercise.views.objects.create()
-            x.save()
-        else:
-            x=Exercise.views.objects.all()[0]
-            x.hits=x.hits+1
-            x.save()
-        context={'videoviews':x.hits}
-        return render(self, 'video.html',context=context)
+    #     if(Exercise.views.objects.count()<=0):
+    #         x=Exercise.views.objects.create()
+    #         x.save()
+    #     else:
+    #         x=Exercise.views.objects.all()[0]
+    #         x.hits=x.hits+1
+    #         x.save()
+    #     context={'videoviews':x.hits}
+    #     return render(self, 'video.html',context=context)
 class SearchResultsView(ListView):
     model = Exercise
     template_name= 'search_results.html'
@@ -91,17 +91,7 @@ class SearchResultsView(ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         return Exercise.objects.filter(exercise_name__icontains=query)
-def VideoViews(self):
 
-    if(Exercise.views.objects.count()<=0):
-        x=Exercise.views.objects.create()
-        x.save()
-    else:
-        x=Exercise.views.objects.all()[0]
-        x.hits=x.hits+1
-        x.save()
-    context={'videoviews':x.hits}
-    return render(self, 'video.html',context=context)
 
 
 # class VideoView(generic.ListView):
