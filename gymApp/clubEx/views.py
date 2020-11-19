9673aff50db15e300e7942b9704f810decc52bf4
William Lucian Earl <92060031@learn.cpp.ac.nz>
2020-11-19 15:00:09 +1300

    sorted vids by views and added featured video

--- gymApp/clubEx/views.py
+++ gymApp/clubEx/views.py
@@ -28,7 +28,7 @@ def subscriptions(request):
         form = AccountForm(request.POST)
         if form.is_valid():
             f = form.cleaned_data['addSub']
-            name = request.user 
+            name = request.user
             name.subscription_plan = f
             name.is_subscribed = True
             name.save()
@@ -41,15 +41,16 @@ def subscriptions(request):
 @login_required
 def category(request, pk):
     ca_exercise = Exercise.objects.filter(category=pk).order_by('exercise_name')
-    
+
     categories = Category.objects.all().order_by('category_name')
     return render(request, 'categories.html', {'pk':pk,'ca_exercise':ca_exercise, 'categories':categories})
 
 @login_required
 def exercises(request):
-    category_exercise = Exercise.objects.all().order_by('exercise_name')
+    category_exercise = Exercise.objects.all().order_by('-views')
+    featured_exercise = Exercise.objects.all()
     categories = Category.objects.all().order_by('category_name')
-    return render(request, 'exercise.html', {'category_exercise':category_exercise, 'categories':categories})
+    return render(request, 'exercise.html', {'category_exercise':category_exercise, 'categories':categories, 'featured_exercise':featured_exercise,})
 
 @login_required
 def complete(request):
@@ -57,17 +58,17 @@ def complete(request):
 
 
 @login_required
-def upload(request):  
-    if request.method == 'POST':  
+def upload(request):
+    if request.method == 'POST':
         form=UploadForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
 
-        handle_uploaded_file(request.FILES['videofile'])  
-        return HttpResponseRedirect('/exercises/')  
-    else:  
-        video = UploadForm()  
-        return render(request,"upload.html",{'form':video})  
+        handle_uploaded_file(request.FILES['videofile'])
+        return HttpResponseRedirect('/exercises/')
+    else:
+        video = UploadForm()
+        return render(request,"upload.html",{'form':video})
 
 def videoDetail(request, pk):
     object = Exercise.objects.get(pk=pk)
