ef7e556d92ffe5f000d65665a33da2a70b26beb5
Troy Brown <90054008@learn.cpp.ac.nz>
2020-11-25 14:27:35 +1300

    Adding more comments

--- gymApp/Account/admin.py
+++ gymApp/Account/admin.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 """
+---------------------------------------------------------------------------------------------------------------------------------------------
 django.contrib consists of common packages such as models
-which can also be used for database tables.
+which are also used for database tables.
+---------------------------------------------------------------------------------------------------------------------------------------------
 """
