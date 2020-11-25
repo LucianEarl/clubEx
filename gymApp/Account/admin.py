fbb3ad6ab7c3979e3533e0ced60aa49cfb2da9db
Troy Brown <90054008@learn.cpp.ac.nz>
2020-11-25 13:58:52 +1300

    Making comments to the account

--- gymApp/Account/admin.py
+++ gymApp/Account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+"""
+django.contrib consists of common packages such as models
+which can also be used for database tables.
+"""
--- gymApp/clubEx/apps.py
+++ gymApp/clubEx/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class ClubexConfig(AppConfig):
     name = 'clubEx'
+
+"""
+django.apps is a registry of installed applications that stores
+configuration. 
+AppConfig is a configuration within the registry
+that is being imported.
+ClubexConfig is a class that is being assigned with a variable.
+"""
